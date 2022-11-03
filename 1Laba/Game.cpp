f6ad7f614a68377701c6d1e297bf0877bc432b99
Pavel <castle_clash2@mail.ru>
2022-11-03 13:12:53 +0300

    Добавьте файлы проекта.

---
+++ 1Laba/Game.cpp
@@ -0,0 +1,139 @@
+﻿#include "Game.h"
+
+void krestiki_noliki::SetLength(int x_length, int y_length) {
+	cin >> x_length >> y_length;
+	x_length_p = x_length;
+	y_length_p = y_length;
+}
+int krestiki_noliki::GetLength(int x_length, int y_length) {
+	x_length = x_length_p;
+	y_length = y_length_p;
+	return x_length, y_length;
+}
+void krestiki_noliki::init(int x_length, int y_length) {
+	cout << "vvedite correct size polya";
+	SetLength(x_length, y_length);
+	x_length = GetLength(x_length, y_length);
+	y_length = GetLength(x_length, y_length);
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(32767, '\n');
+		system("clear");
+		init(x_length, y_length);
+	}
+	for (size_t i = 0; i < x_length; ++i)
+	{
+		for (size_t j = 0; j < y_length; ++j)
+		{
+			temp.push_back("-");
+		}
+		field_p.push_back(temp);
+		temp.clear();
+	}
+}
+void krestiki_noliki::paint(int x_length, int y_length) {
+	for (size_t i = 0; i < x_length; ++i)
+	{
+		for (size_t j = 0; j < y_length; ++j)
+		{
+			cout << field_p[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void krestiki_noliki::check_win(bool* check, int x_length, int y_length) {
+	x_length, y_length = GetLength(x_length, y_length);
+	for (size_t i = 0; i < x_length; ++i)
+	{
+		for (size_t j = 0; j < y_length; ++j) {
+			if ((field_p[i][j] == "X" || field_p[i][j] == "O"))
+			{
+				if (j != 0 && j != x_length - 1)
+				{
+					if (field_p[i][j] == field_p[i][j - 1] && field_p[i][j] == field_p[i][j + 1])
+					{
+						cout << endl << "Win " << field_p[i][j];
+						*check = true;
+					}
+				}
+				if (i != 0 && i != y_length - 1) 
+				{
+					if (field_p[i][j] == field_p[i - 1][j] && field_p[i][j] == field_p[i + 1][j])
+					{
+						cout << endl << "Win " << field_p[i][j];
+						*check = true;
+					}
+				}
+				if (i != 0 && j != 0 && i != y_length - 1 && j != x_length - 1)
+				{
+					if (field_p[i][j] == field_p[i - 1][j - 1] && field_p[i][j] == field_p[i + 1][j + 1]) // crush
+					{
+						cout << endl << "Win " << field_p[i][j];
+						*check = true;
+					}
+				}
+				if (i != 0 && j != 0 && i != y_length - 1 && j != x_length - 1) 
+				{
+					if (field_p[i][j] == field_p[i - 1][j + 1] && field_p[i][j] == field_p[i + 1][j - 1])
+					{
+						cout << endl << "Win " << field_p[i][j];
+						*check = true;
+					}
+				}
+			}
+		}
+	}
+}
+int krestiki_noliki::turn_cross(int x_length, int y_length) {
+	cout << "seichas xod igroka isp krestiki" << endl << "vvedite pizition na kotoryu xotite poxodit";
+	correct_input(&position_y, &position_x, x_length, y_length);
+	place_cross(position_y, position_x, x_length, y_length);
+	system("clear");
+	paint(x_length, y_length);
+	return 2;
+}
+void krestiki_noliki::place_cross(int position_y, int position_x, int x_length, int y_length) {
+	field_p[position_y - 1][position_x - 1] = "X";
+}
+int krestiki_noliki::turn_zero(int x_length, int y_length) {
+	cout << "Now it's the player's turn using the zeroes" << endl << "Enter the position you want to be";
+	correct_input(&position_y, &position_x, x_length, y_length);
+	field_p[position_y - 1][position_x - 1] = "O";
+	system("clear");
+	paint(x_length, y_length);
+	return 1;
+}
+void krestiki_noliki::place_zero(int position_y, int position_x, int x_length, int y_length) {
+	field_p[position_y - 1][position_x - 1] = "O";
+}
+void krestiki_noliki::input_error(int* y_pos, int* x_pos, int x_length, int y_length) {
+	system("clear");
+	paint(x_length, y_length);
+	cout << "Enter a valid move position" << endl;
+	correct_input(&position_y, &position_x, x_length, y_length);
+}
+void krestiki_noliki::InputTypeCheck(int* y_pos, int* x_pos) {
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(32767, '\n');
+	}
+}
+void krestiki_noliki::correct_input(int* y_pos, int* x_pos, int x_length, int y_length) {
+	cin >> position_y >> position_x;
+	InputTypeCheck(&position_y, &position_x);
+	if (0 < position_y && position_y <= y_length && 0 < position_x && position_x <= x_length && field_p[position_y - 1][position_x - 1] == "-")
+	{
+		paint(x_length, y_length);
+		exit;
+	}
+	else
+	{
+		input_error(&position_y, &position_x, x_length, y_length);
+	}
+}
+const void krestiki_noliki::operator()(const double num1, const double num2)
+{
+	cout << field_p[num1][num2] << endl;	
+}
---
+++ 1Laba/Game.h
@@ -0,0 +1,97 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <typeinfo>
+
+using std::cout;
+using std::cin;
+using std::vector;
+using std::string;
+using std::endl;
+/*
+	\Krestiki_noliki A class that represents an implementation of the Tic-Tac-Toe game
+
+	This class has a full set of functions necessary for a full and comfortable game: the choice of field sizes, the ability to set a symbol,
+checking the input of all values ​​that are set by the user, as well as checking the victory of one of the parties
+*/
+class krestiki_noliki {
+private:
+	vector <vector<string>> field_p;
+	vector<string> temp;
+	string position;
+	char x;
+	int x_length_p, y_length_p, position_x, position_y;
+public:
+	void SetLength(int x_length_, int y_length_);
+	/* Sets the size of the playing field
+		\param[x_length] x_length The size of the playing field horizontally
+		\param[y_length] y_length The size of the playing field vertical
+	*/
+	void InputTypeCheck(int* y_pos, int* x_pos);
+	/* Checking the input data type
+	   \param[y_pos] position_y The horizontal coordinates of the cell in which you want to set the cross
+		\param[x_pos] position_x The vertical coordinates of the cell in which you want to set the cross
+	*/
+	void correct_input(int* y_pos, int* x_pos, int x_length, int y_length);
+	/* Getting data from the user and checking the cell
+		\param[y_pos] position_y Horizontal coordinates of the cell where the cross should be placed
+		\param[x_pos] position_x Vertical coordinates of the cell where the cross should be placed
+		\param[x_length] x_length Horizontal size of the playing field
+		\param[y_length] y_length Vertical size of the playing field
+	*/
+	void init(int x_length, int y_length);
+	/* Match initialization, playing field creation
+		\param[x_length] x_length Horizontal size of the playing field
+		\param[y_length] y_length Vertical size of the playing field
+	*/
+	void paint(int x_length, int y_length);
+	/* Displaying the current state of the playing field on the screen
+		\param[x_length] x_length Horizontal size of the playing field
+		\param[y_length] y_length Vertical size of the playing field
+	*/
+	void check_win(bool* check, int x_length, int y_length);
+	/* Checking for the victory of one of the parties
+		\param[check] check victory status indicator (0 - neither side wins, 1 - one side wins)
+		\param[x_length] x_length Horizontal size of the playing field
+		\param[y_length] y_length Vertical size of the playing field
+	*/
+	int turn_cross(int x_length, int y_length);
+	/* Checking the progress of the crosses
+		\param[x_length] x_length Horizontal size of the playing field
+	\param[y_length] y_length Vertical size of the playing field
+	*/
+	void place_cross(int position_y, int position_x, int x_length, int y_length);
+	/* 
+		Setting the cross to the specified position
+		\param[position_y] position_y Horizontal coordinates of the cell where the cross should be placed
+		\param[position_x] position_x Vertical coordinates of the cell where the cross should be placed
+		\param[x_length] x_length Horizontal size of the playing field
+		\param[y_length] y_length Vertical size of the playing field
+	*/
+	int GetLength(int x_length, int y_length);
+	/* Getting values ​​for the size of the playing field
+\		param[x_length] x_length Horizontal size of the playing field
+\		param[y_length] y_length Vertical size of the playing field
+	*/
+	void input_error(int* y_pos, int* x_pos, int x_length, int y_length);
+	/* Displaying an error about incorrect input and prompting for re-entry
+\		param[y_pos] position_y Horizontal coordinates of the cell where the cross should be placed
+\		param[x_pos] position_x Vertical coordinates of the cell where the cross should be placed
+\		param[x_length] x_length Horizontal size of the playing field
+\		param[y_length] y_length Vertical size of the playing field
+	*/
+	int turn_zero(int x_length, int y_length);
+	/* Zero travel checks
+\		param[x_length] x_length Horizontal size of the playing field
+\		param[y_length] y_length Vertical size of the playing field
+	*/
+	void place_zero(int position_y, int position_x, int x_length, int y_length);
+	/* Setting the zero to the specified position
+\		param[position_y] position_y Horizontal coordinates of the cell in which you want to set the zero
+\		param[position_x] position_x Vertical coordinates of the cell in which you want to set the zero
+\		param[x_length] x_length Horizontal size of the playing field
+\		param[y_length] y_length Vertical size of the playing field
+	*/
+	const void operator() (const double num1, const double num2); //Overload
+};
